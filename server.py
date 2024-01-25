d3e5e6464e6785557014d288e97d85e4c047a72c
noircodes <ilhamizdhihar2007@gmail.com>
2024-01-25 11:39:21 +0700

    modify server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import uvicorn
+from main import app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=3000, proxy_headers=True, forwarded_allow_ips="*")
+    uvicorn.run(app, host="0.0.0.0", port=3000, proxy_headers=True, forwarded_allow_ips="*", loop="none")
